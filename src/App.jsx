04806eb4d72dc935c8359dd630770465bbc843e8
Shaurya <shaurya.deoria@gmail.com>
2024-11-16 03:46:07 +0530

    Refactor App component layout and update CSS for improved styling and positioning

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar'
-import ComingSoon from './components/Comingsoon'
-import FestName from './components/FestName'
+import { useState } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import ComingSoon from './components/Comingsoon'; // Assuming ComingSoon component exists
+import FestName from './components/FestName'; // Assuming FestName component exists
 
 function App() {
   return (
     <>
       <Navbar />
-      <ComingSoon />
-      <FestName />
-
+      <div className="navbar-space">
+        <img src="/cs2.png" alt="CS2" className="coming-soon" />
+        <img src="/image_Yuvaan.png" alt="Yuvaan" className="image-yuvaan" />
+      </div>
       <div>
-        {[...Array(100)].map((_, i) => (
+        {[...Array(140)].map((_, i) => (
           <br key={i} />
         ))}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
