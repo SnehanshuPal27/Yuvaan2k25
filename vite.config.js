fd642782964e5d35e3cfbcaf6d2fffedcf99d197
SnehanshuPalGrig <snehanshu.pal@grigtechnologies.com>
2025-02-09 12:05:04 +0530

    gh config try

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,17 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss(),],
+  base: '/Yuvaan2k25/',
+  build: {
+    assetsDir: '',  // This is important - it will put assets at root level
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          // Preserve the original path structure
+          return `[name][extname]`;
+        },
+      }
+    }
+  }
 })
